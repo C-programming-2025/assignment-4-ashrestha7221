6d8f90d6db292b0e0447380f4f931f0e115ae5f3
ashrestha7221 <a.shrestha.7221@westcliff.edu>
2025-12-17 10:23:29 +0545

    Update q12.c

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,25 @@
 // Write a C program to read the contents of a text file and display them on the screen.
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+
+    fp = fopen("output.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: Could not open the file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        putchar(ch);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
