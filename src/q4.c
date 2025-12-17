119a4c3589cfa6aeabeaa542231a277ce80f26ee
ashrestha7221 <a.shrestha.7221@westcliff.edu>
2025-12-17 10:36:19 +0545

    Update q4.c

--- src/q4.c
+++ src/q4.c
@@ -1,1 +1,36 @@
 // Write a C program that dynamically allocates memory for an integer array of size n, where n is entered by the user. Prompt the user to enter n elements and display them using pointers.
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main()
+{
+    int n, i;
+    int *arr;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", arr + i); 
+    }
+
+    printf("The elements of the array are:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    free(arr);
+
+    return 0;
+}
