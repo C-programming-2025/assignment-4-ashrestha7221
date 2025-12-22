a092d4b1f517e603c4c9f1233a294a3d1288ba3f
ashrestha7221 <a.shrestha.7221@westcliff.edu>
2025-12-22 21:21:09 +0545

    Update q8.c

--- src/q8.c
+++ src/q8.c
@@ -1,3 +1,49 @@
-// Write a C program that defines a structure Book with members: title, author, and price. 
-// Allow the user to enter details of n books and then display all books whose price is above a 
-// user-defined value.
+#include <stdio.h>
+
+struct Book
+{
+    char title[100];
+    char author[100];
+    float price;
+};
+
+int main()
+{
+    struct Book books[50];
+    int n, i;
+    float limit;
+
+    printf("Enter number of books: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("\nEnter details of Book %d\n", i + 1);
+
+        printf("Title: ");
+        scanf(" %[^\n]", books[i].title);
+
+        printf("Author: ");
+        scanf(" %[^\n]", books[i].author);
+
+        printf("Price: ");
+        scanf("%f", &books[i].price);
+    }
+
+    printf("\nEnter price limit: ");
+    scanf("%f", &limit);
+
+    printf("\n--- Books with price above %.2f ---\n", limit);
+
+    for (i = 0; i < n; i++)
+    {
+        if (books[i].price > limit)
+        {
+            printf("\nTitle: %s", books[i].title);
+            printf("\nAuthor: %s", books[i].author);
+            printf("\nPrice: %.2f\n", books[i].price);
+        }
+    }
+
+    return 0;
+}
